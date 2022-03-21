fde6d232e4059e04322113fe5685cf2569ce9318
Mahmoud Gamal <Mahmoudgamal7575@gmail.com>
2022-03-21 15:38:05 +0200

    CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+import { RouterModule, Routes, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AllInterfacesDetailsComponent } from './all-interfaces-details/all-interfaces-details.component';
 import { AppConfigurationComponent } from './app-configuration/app-configuration.component';
 import { BusinessUnitComponent } from './business-unit/business-unit.component';
@@ -13,26 +15,35 @@ import { MappingComponent } from './mapping/mapping.component';
 import { NewInterfaceComponent } from './new-interface/new-interface.component';
 import { SympDbComponent } from './symp-db/symp-db.component';
 
-
+export class alwaysAuthGuard implements CanActivate{
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    let x=["NewInterface","mapping"]
+    for (let i = 0; i < x.length; i++) {
+      if (route.routeConfig?.path==x[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 const routes: Routes = [
   {path:"",redirectTo:"home",pathMatch:"full"},
-  {path:"data",component: DataComponent},
-  {path:"mapping",component: MappingComponent},
-  {path:"sympdb",component: SympDbComponent},
-  {path:"home",component: HomeComponent},
-  {path:"BU",component:BusinessUnitComponent},
-  {path:"APPConfig",component:AppConfigurationComponent},
-  {path:"homeConfig",component:HomeConfigComponent},
-  {path:"NewInterface",component:NewInterfaceComponent},
-  {path:"allInterfacesDetails",component:AllInterfacesDetailsComponent},
-  {path:"MappingDetails",component:MappingDetailComponent},
-  {path:"MappingView",component:MappingViewComponent},
+  {path:"data",component: DataComponent,canActivate:[alwaysAuthGuard]},
+  {path:"mapping",component: MappingComponent,canActivate:[alwaysAuthGuard]},
+  {path:"sympdb",component: SympDbComponent,canActivate:[alwaysAuthGuard]},
+  {path:"home",component: HomeComponent,canActivate:[alwaysAuthGuard]},
+  {path:"BU",component:BusinessUnitComponent,canActivate:[alwaysAuthGuard]},
+  {path:"APPConfig",component:AppConfigurationComponent,canActivate:[alwaysAuthGuard]},
+  {path:"homeConfig",component:HomeConfigComponent,canActivate:[alwaysAuthGuard]},
+  {path:"NewInterface",component:NewInterfaceComponent,canActivate:[alwaysAuthGuard]},
+  {path:"allInterfacesDetails",component:AllInterfacesDetailsComponent,canActivate:[alwaysAuthGuard]},
+  {path:"MappingDetails",component:MappingDetailComponent,canActivate:[alwaysAuthGuard]},
+  {path:"MappingView",component:MappingViewComponent,canActivate:[alwaysAuthGuard]},
   {path:"License",component:LicenseComponent}
-
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,alwaysAuthGuard} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -21,6 +21,7 @@ import { MappingViewComponent } from './mapping-view/mapping-view.component';
 import { MappingDetailComponent } from './mapping-detail/mapping-detail.component';
 import { LicenseComponent } from './license/license.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -52,7 +53,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     
   ],
-  providers: [],
+  providers: [alwaysAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
