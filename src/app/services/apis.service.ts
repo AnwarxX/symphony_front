d51c333e827294ec5457cb425d8b665d17149483
lamiaa <lamiaa@gmail.com>
2022-03-29 14:52:36 +0200

    front

--- src/app/services/apis.service.ts
+++ src/app/services/apis.service.ts
@@ -4,13 +4,25 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class APIsService {
   baseURL="http://localhost:5000/"
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+   
+
+  }
   getFun(endPoint:any){
     return this.http.get<any>(this.baseURL+endPoint)
   }
   postFun(endPoint:any,body:any){
     return this.http.post<any>(this.baseURL+endPoint,body)
   }
+
+ 
 }
+
+ 
+
+
+
