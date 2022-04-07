0265742c355dad5bd311d12576009da3b3081c44
lamiaa <lamiaa@gmail.com>
2022-04-07 13:14:09 +0200

    nav fix2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { interval } from 'rxjs';
+import * as cryptoJS from 'crypto-js';
 declare var $:any //declear $ to use jquery
 
 @Component({
@@ -8,8 +10,13 @@ declare var $:any //declear $ to use jquery
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  remainingTime:any;
+  showTimer=true
+  constructor(private router: Router) {
+    this.timeCounter()
+    interval(1000).subscribe((ev)=>{
+      this.timeCounter()
+    })   }
 
   ngOnInit(): void {
     //add the  background to home id
@@ -19,5 +26,32 @@ export class FooterComponent implements OnInit {
   });
 
   }
-
+  
+  timeCounter(){
+    try {
+      let tok =  localStorage.getItem('token');
+      var bytes  = cryptoJS.AES.decrypt(tok||"", 'lamiaa');
+      var originalText = bytes.toString(cryptoJS.enc.Utf8);
+      if(originalText.includes("EndDate")){
+        this.showTimer=true
+        let data=JSON.parse(originalText)
+        var seconds = Math.floor((new Date(data[0].EndDate).getTime() - (new Date().getTime()))/1000);
+        var minutes = Math.floor(seconds/60);
+        var hours = Math.floor(minutes/60);
+        var days = Math.floor(hours/24);
+    
+        hours = hours-(days*24);
+        minutes = minutes-(days*24*60)-(hours*60);
+        seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+        this.remainingTime=days+"D"+" : "+hours+"H"+" : "+minutes+"M"+" : "+seconds+"S";
+        console.log(this.remainingTime);
+      }
+      else
+      {
+        this.showTimer=false
+      }
+    } catch (error:any) {
+      console.log(error.message);
+    }
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { interval } from 'rxjs';
-import * as cryptoJS from 'crypto-js';
 
 declare var $:any //declear $ to use jquery
 
@@ -12,13 +10,7 @@ declare var $:any //declear $ to use jquery
 })
 export class NavbarComponent implements OnInit {
   isShown: boolean = false ;
-  remainingTime:any;
-  showTimer=true
-  constructor(private router: Router) {
-    this.timeCounter()
-    interval(1000).subscribe((ev)=>{
-      this.timeCounter()
-    })     
+  constructor(private router: Router) {   
     this.router.events.subscribe((ev) => {
       if (ev instanceof NavigationEnd) {
       if (this.router.url != "/home" && this.router.url != "/License") {
@@ -40,32 +32,5 @@ export class NavbarComponent implements OnInit {
       lineColor: 'white'
   });
 
-  }
-  timeCounter(){
-    try {
-      let tok =  localStorage.getItem('token');
-      var bytes  = cryptoJS.AES.decrypt(tok||"", 'lamiaa');
-      var originalText = bytes.toString(cryptoJS.enc.Utf8);
-      if(originalText.includes("EndDate")){
-        this.showTimer=true
-        let data=JSON.parse(originalText)
-        var seconds = Math.floor((new Date(data[0].EndDate).getTime() - (new Date().getTime()))/1000);
-        var minutes = Math.floor(seconds/60);
-        var hours = Math.floor(minutes/60);
-        var days = Math.floor(hours/24);
-    
-        hours = hours-(days*24);
-        minutes = minutes-(days*24*60)-(hours*60);
-        seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
-        this.remainingTime=days+"D"+" : "+hours+"H"+" : "+minutes+"M"+" : "+seconds+"S";
-        console.log(this.remainingTime);
-      }
-      else
-      {
-        this.showTimer=false
-      }
-    } catch (error:any) {
-      console.log(error.message);
-    }
   }
 }
