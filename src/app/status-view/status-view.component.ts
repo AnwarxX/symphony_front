95e176f306318d02641db50528791a8c855dd9ec
Zero <mahmoudgamal7575@gmail.com>
2022-05-16 12:03:08 +0200

    status import

--- src/app/status-view/status-view.component.ts
+++ src/app/status-view/status-view.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { APIsService } from '../services/apis.service';
+declare var $:any //declear $ to use jquery
 
 @Component({
   selector: 'app-status-view',
   templateUrl: './status-view.component.html',
   styleUrls: ['./status-view.component.css']
 })
+
 export class StatusViewComponent implements OnInit {
   statData:any;
+  input:any;
   constructor(public apiService:APIsService) {
     this.statusData("")
   }
@@ -16,18 +19,29 @@ export class StatusViewComponent implements OnInit {
   }
   async statusData(date:any){
     this.apiService.postFun('statusData',{date:date}).subscribe(data => {
+      console.log(data);
       this.statData = data;
     })
   }
   search(event:any){
-    console.log((<HTMLInputElement>event.target).value);
+    this.input=(<HTMLInputElement>event.target).value;
     this.statusData((<HTMLInputElement>event.target).value)
 }
-  async importFailed(api:any,date:any,interfaceCode:any){
-    this.apiService.postFun('import', { api,date: date.split("T")[0], interface: interfaceCode }).subscribe(data => {
-      setTimeout(() => {
-        this.statusData("")
-      }, 2000);
+  async importFailed(row:any){
+    let endpoint;
+    if (row.ApiName.includes('caps')) {
+      endpoint='capsImport'
+    }
+    else{
+      endpoint='import'
+    }    
+    this.apiService.postFun(endpoint, { api:row.ApiName.split('-')[0],date: row.Date.split("T")[0], interface: row.interfaceCode }).subscribe(data => {
+      // setTimeout(() => {
+        this.statusData(this.input)
+      // }, 2000);
+      console.log(data);
+      $('#liveToast').toast('show')
+      $('.toast-body').text(data)
     })
   }
 }
